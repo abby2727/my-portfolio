4ff9bfcdd429023b31d2b04fb1e8ee1fa61fc5fb
abby2727 <abdulfahadgo566@gmail.com>
2022-08-04 11:58:04 +0800

    Added My Projects

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
   $(window).scroll(function () {
-    // sticky navbar on scroll script
+    // Sticky navbar on scroll script
     if (this.scrollY > 20) {
       $(".navbar").addClass("sticky");
     } else {
       $(".navbar").removeClass("sticky");
     }
 
-    // scroll-up button show/hide script
+    // Scroll-up button show/hide script
     if (this.scrollY > 500) {
       $(".scroll-up-btn").addClass("show");
     } else {
@@ -15,44 +15,21 @@ $(document).ready(function () {
     }
   });
 
-  // slide-up script
+  // Slide-up script
   $(".scroll-up-btn").click(function () {
     $("html").animate({ scrollTop: 0 });
-    // removing smooth scroll on slide-up button click
+    // Removing smooth scroll on slide-up button click
     $("html").css("scrollBehavior", "auto");
   });
 
   $(".navbar .menu li a").click(function () {
-    // applying again smooth scroll on menu items click
+    // Applying again smooth scroll on menu items click
     $("html").css("scrollBehavior", "smooth");
   });
 
-  // toggle menu/navbar script
+  // Toggle menu/navbar script
   $(".menu-btn").click(function () {
     $(".navbar .menu").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
-  });
-
-  // owl carousel script
-  $(".carousel").owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplay: true,
-    autoplayTimeOut: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false,
-      },
-      600: {
-        items: 2,
-        nav: false,
-      },
-      1000: {
-        items: 3,
-        nav: false,
-      },
-    },
   });
 });
