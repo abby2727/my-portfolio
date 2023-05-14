c34fc01fdaf572b28eb34dfd9ea526f503478fd6
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-05-14 18:35:18 +0800

    Clean and update

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
 $(document).ready(function () {
-  $(window).scroll(function () {
-    // Sticky navbar on scroll script
-    if (this.scrollY > 20) {
-      $(".navbar").addClass("sticky");
-    } else {
-      $(".navbar").removeClass("sticky");
-    }
 
-    // Scroll-up button show/hide script
-    if (this.scrollY > 500) {
+  $(window).scroll(function () {
+    if (this.scrollY > 20)
+      $(".navbar").addClass("sticky");
+    else
+      $(".navbar").removeClass("sticky");
+
+    if (this.scrollY > 500)
       $(".scroll-up-btn").addClass("show");
-    } else {
+    else
       $(".scroll-up-btn").removeClass("show");
-    }
   });
 
-  // Slide-up script
   $(".scroll-up-btn").click(function () {
     $("html").animate({ scrollTop: 0 });
-    // Removing smooth scroll on slide-up button click
     $("html").css("scrollBehavior", "auto");
   });
 
   $(".navbar .menu li a").click(function () {
-    // Applying again smooth scroll on menu items click
     $("html").css("scrollBehavior", "smooth");
   });
 
-  // Toggle menu/navbar script
   $(".menu-btn").click(function () {
     $(".navbar .social").toggleClass("active");
     $(".navbar .menu").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
   });
+
 });
